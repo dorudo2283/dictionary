13bb58ce4e97343d72af5a0a6784172284bf11ae
Dorudo <synk8824@gmail.com>
2026-06-27 09:40:59 +0900

    data-eiwa.js を作成

---
+++ data-eiwa.js
@@ -0,0 +1,4 @@
+const dictEiwa = {
+    "東京": "<p><b>英訳:</b> Tokyo</p><p><b>品詞:</b> 固有名詞</p><p><b>用例:</b> I live in Tokyo.（私は東京に住んでいます）</p>",
+    "令和": "<p><b>英訳:</b> Reiwa</p><p><b>品詞:</b> 固有名詞</p><p><b>説明:</b> The current era of Japan.</p>"
+};
